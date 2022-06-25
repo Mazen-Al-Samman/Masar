edc3c0324e030693bf769d5f241400dd61d6ed46
Mazen AlSamman <batman@pop-os.localdomain>
2022-06-25 18:54:13 +0300

    New font for arabic

---
+++ common/translation.ts
@@ -0,0 +1,21 @@
+const en = {
+    'home': 'Home',
+    'allCompanies': 'All Companies',
+    'aboutUs': 'About Us',
+    'contact': 'Contact Us',
+    'search': 'Search for companies'
+};
+
+const ar = {
+    'home': 'الرئيسية',
+    'allCompanies': 'كل الشركات',
+    'aboutUs': 'من نحن',
+    'contact': 'تواصل معنا',
+    'search': 'ابحث عن شركات'
+};
+
+const translations = {
+    en: en,
+    ar: ar
+};
+export default translations;
