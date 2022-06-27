765bce6ff19ed474f3f1ed7bb8103d91295a293e
Mazen AlSamman <batman@pop-os.localdomain>
2022-06-27 21:02:41 +0300

    Change file structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import NavBar from '../common/NavBar';
+import NavBar from '../components/NavBar';
 import translation from '../common/translation';
 
 interface IProps {
