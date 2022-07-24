67d25de1d5788c30612c3d9e63fdcc2dd6cccf02
Mazen AlSamman <batman@pop-os.localdomain>
2022-07-24 23:28:57 +0300

    Use Bootstrap with companies boxes

--- pages/super-admin/companies/index.tsx
+++ pages/super-admin/companies/index.tsx
@@ -1,27 +1,38 @@
 import Details from "../../../components/companies/Details";
 import {HandleRequestSSR} from "../../api/Handler";
+import {Col, Container, Row} from 'react-bootstrap';
 
 const Companies = ({data}: any) => {
     console.log("data", data)
     return (
-        <div style={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
-            <Details type={`new`}></Details>
-            {
-                data &&
-                data.map((company: any) => {
-                    const {title, description, number_of_employees, country, city, logo} = company;
-                    return <Details type={`company`} data={
-                        {
-                            title,
-                            description,
-                            number_of_employees,
-                            country,
-                            city,
-                            logo,
-                        }
-                    }></Details>
-                })
-            }
+        <div>
+            <Container>
+                <Row style={{display: 'flex', justifyContent: 'center'}}>
+                    <Col lg={5} style={{margin: '0 14px 30px 14px'}}>
+                        <Details type={`new`}></Details>
+                    </Col>
+                    {
+                        data &&
+                        data.map((company: any) => {
+                            const {title, description, number_of_employees, country, city, logo} = company;
+                            return (
+                                <Col lg={5} style={{margin: '0 14px 30px 14px'}}>
+                                    <Details type={`company`} data={
+                                        {
+                                            title,
+                                            description,
+                                            number_of_employees,
+                                            country,
+                                            city,
+                                            logo,
+                                        }
+                                    }></Details>
+                                </Col>
+                            )
+                        })
+                    }
+                </Row>
+            </Container>
         </div>
     )
 }
