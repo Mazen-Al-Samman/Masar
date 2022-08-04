b79541499bc783dd0e0dcf55f01b8e7b11c0726f
batman <MohammadMazen.AlSamman@opensooq.com>
2022-08-05 00:26:55 +0300

    Handle empty results message

--- pages/super-admin/companies/index.tsx
+++ pages/super-admin/companies/index.tsx
@@ -5,13 +5,21 @@ import {useLayoutEffect} from "react";
 import MainProps from "../../../interfaces/MainProps";
 import {v4 as uuidV4} from 'uuid';
 import Image from "next/image";
-import { useRouter } from 'next/router';
+import {useRouter} from 'next/router';
 
 interface PageProps {
     filtersData: object,
 }
 
-const Companies = ({data, setButtons, filtersData, setFilter, selected, setSelected, search}: (MainProps & PageProps)) => {
+const Companies = ({
+                       data,
+                       setButtons,
+                       filtersData,
+                       setFilter,
+                       selected,
+                       setSelected,
+                       search
+                   }: (MainProps & PageProps)) => {
     // @ts-ignore
     let filteredCompanies = [...data];
     for (let selectedFilter in selected) {
@@ -58,6 +66,24 @@ const Companies = ({data, setButtons, filtersData, setFilter, selected, setSelec
         <div>
             <Container>
                 <Row style={{display: 'flex', justifyContent: 'center'}}>
+                    {
+                        filteredCompanies.length == 0 &&
+                        <Col lg={12} style={{marginBottom: '10px'}}>
+                            <div style={{display: 'flex', columnGap: '20px', alignItems: 'center'}}>
+                                <Image src={`/icons/search.svg`} width={56} height={56} alt={`Search Icon`}></Image>
+                                <div style={{marginTop: '25px'}}>
+                                    <p style={{fontSize: '20px', fontWeight: '700'}}>Sorry! We couldn't find what you're
+                                        looking for</p>
+                                    <p style={{
+                                        fontSize: '16px',
+                                        fontWeight: '400',
+                                        marginTop: '-6px',
+                                        color: '#04252E'
+                                    }}>Clear filters to see all results again.</p>
+                                </div>
+                            </div>
+                        </Col>
+                    }
                     <Col lg={12}
                          style={{marginBottom: '10px', display: 'flex', justifyContent: 'left'}}>
                         {
@@ -101,7 +127,8 @@ const Companies = ({data, setButtons, filtersData, setFilter, selected, setSelec
                         filteredCompanies.map((company: any) => {
                             const {title, description, number_of_employees, country, city, logo} = company;
                             return (
-                                <Col onClick={() => redirectTo(`companies/${company.id}`)} key={uuidV4()} lg={6} sm={12} md={12} style={{marginBottom: '24px', cursor: "pointer"}}>
+                                <Col onClick={() => redirectTo(`companies/${company.id}`)} key={uuidV4()} lg={6} sm={12}
+                                     md={12} style={{marginBottom: '24px', cursor: "pointer"}}>
                                     <Details type={`company`} data={
                                         {
                                             title,
